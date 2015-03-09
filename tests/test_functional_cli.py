20b9f45e08ac3d258a69d1bb3fb00c08da0215a5
ivan sabik <ivan.rodriguez.o@gmail.com>
2015-03-09 14:26:26 -0600

    gitignore update for temps

---
+++ tests/test_functional_cli.py
@@ -0,0 +1,17 @@
+import unittest
+
+class CliFunctionalTest(unittest.TestCase):
+    def setUp(self):
+        'nothing yet here'
+
+    def tearDown(self):
+        'nothing yet here'
+
+    def test_generate_random_pass(self):
+        self.fail('Finish test to generate random password!')
+
+    def test_generate_img_for_input_pass(self):
+        self.fail('Finish test to generate image from text password!')
+
+if __name__ == '__main__':
+    unittest.main()
